9e34b8f6650337ad9b10a5a88ab3d9c42ab5ed56
torohash <torohash@gmail.com>
2025-03-26 12:19:19 +0000

    npm使うように修正

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 簡易的なTypeScriptファイル
+ * ts-nodeで実行するためのサンプル
+ */
+
+console.log("Hello from playwright-sse-mcp-server!");
+console.log("ts-nodeを使って正常に実行されました。");
+
+// 現在の日時を表示
+const now = new Date();
+console.log(`現在の日時: ${now.toLocaleString()}`);
